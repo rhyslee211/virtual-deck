01db394ea0449334c1e065c1891323a9f055a432
Rhys Lee <78373339+rhyslee211@users.noreply.github.com>
2024-08-07 21:15:44 -0400

    finished windows controls bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="flex flex-col h-screen bg-slate-800">
       <WindowsControls />
       <h1 className="text-sm text-blue-500 font-bold underline">
-        Hello world!!!!
+        Hello world!!!!!!!!!!!!!
       </h1>
     </div>
   );
