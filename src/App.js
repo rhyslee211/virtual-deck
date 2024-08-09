06fec905a1197b08beaf45794961d21b4a4f10a7
Rhys Lee <78373339+rhyslee211@users.noreply.github.com>
2024-08-08 21:07:39 -0400

    started sidebar work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
 import WindowsControls from './components/WindowsControls';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-slate-800">
+    <div className="flex flex-col h-screen bg-slate-700">
       <WindowsControls />
-      <h1 className="text-sm text-blue-500 font-bold underline">
-        Hello world!!!!!!!!!!!!!
-      </h1>
+      <div className="flex flex-row flex-grow">
+        <Sidebar />
+        <div className="flex-grow">
+          <h1 className="text-sm text-blue-500 font-bold underline">
+            Hello world!!!!!!!!!!!!!
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div className="w-16 bg-slate-800">
+            <div className="flex flex-col items-center">
+                <button className="w-12 h-12 mt-4 mb-4 bg-blue-500 rounded-full"></button>
+                <button className="w-12 h-12 mb-4 bg-blue-500 rounded-full"></button>
+                <button className="w-12 h-12 mb-4 bg-blue-500 rounded-full"></button>
+                <button className="w-12 h-12 mb-4 bg-blue-500 rounded-full"></button>
+                <button className="w-12 h-12 mb-4 bg-blue-500 rounded-full"></button>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
